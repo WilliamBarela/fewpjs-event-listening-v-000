c19c623af527faab2b04452afd47df7f0aedfabd
William Barela <william.barela.dev@gmail.com>
2020-01-22 18:19:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function addingEventListener() {
   const input = document.getElementById('input');
-  input.addEventListener('click'
+  input.addEventListener('click',
                           function(event){
                             alert('I was clicked!');
                           }
